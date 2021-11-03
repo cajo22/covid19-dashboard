9d874562b3032aa9b36c6543cb0da1a978f50665
cajo22 <cajo22@protonmail.com>
2021-11-03 14:19:00 +0000

    Write process_covid_csv_data and fix oversight where new cases would be cut off by 1 digit

---
+++ constants.py
@@ -0,0 +1,11 @@
+# CSV related
+ROW_MOST_RECENT_DAY = 1 # First row with data (i.e row 2)
+ROW_FIRST_COMPLETE_DAY = 2 # First row with valid data for the 7-day total (i.e row 3)
+
+COL_AREA_CODE = 0
+COL_AREA_NAME = 1
+COL_AREA_TYPE = 2
+COL_DATE = 3
+COL_TOTAL_DEATHS = 4
+COL_HOSPITAL_CASES = 5
+COL_NEW_CASES = 6
--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,3 +1,5 @@
+from constants import *
+
 def parse_csv_data(csv_filename : str) -> str:
 
     # Each line (row) of file {csv_filename} is appended to a list, not including line breaks.
@@ -6,6 +8,29 @@ def parse_csv_data(csv_filename : str) -> str:
     src_csv = open(csv_filename, "r")
     data = []
     for line in src_csv:
-        data.append(line[:-2])
+        data.append(line[:-1])
     src_csv.close()
     return(data)
+
+def process_covid_csv_data(covid_csv_data : str) -> int:
+    # Takes a list of strings (intended to be the output of parse_csv_data).
+    # Returns three integers: new cases in last 7 days, current hospital cases, total deaths.
+
+    # New cases in the last 7 days
+    # The most recent day (row 1) is invalid, so sum daily cases for rows 2 - 8.
+    cases_7days = 0
+    for i in range(ROW_FIRST_COMPLETE_DAY, ROW_FIRST_COMPLETE_DAY + 7):
+        cases_7days += int((covid_csv_data[i].split(","))[COL_NEW_CASES])
+
+    # Current hospital cases
+    current_hospital_cases = (covid_csv_data[ROW_MOST_RECENT_DAY].split(","))[COL_HOSPITAL_CASES]
+
+    # Total deaths
+    # Continue iterating until the value of 'cumDailyNsoDeathsByDeathDate' is not blank.
+    total_deaths = ""
+    i = ROW_MOST_RECENT_DAY
+    while total_deaths == "":
+        total_deaths = (covid_csv_data[i].split(","))[COL_TOTAL_DEATHS]
+        i += 1
+
+    return cases_7days, int(total_deaths), int(current_hospital_cases)
