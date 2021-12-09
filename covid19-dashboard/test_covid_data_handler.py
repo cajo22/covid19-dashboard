10a7a782e9a1070ee34d701672c9b71e94f62669
cajo22 <cajo22@protonmail.com>
2021-12-09 17:19:00 +0000

    Fix incorrect file paths in testing functions

--- covid19-dashboard/test_covid_data_handler.py
+++ covid19-dashboard/test_covid_data_handler.py
@@ -9,13 +9,13 @@ from covid_data_handler import update_covid_data
 from covid_data_handler import dashboard_process
 
 def test_parse_csv_data():
-    data = parse_csv_data('nation_2021-10-28.csv')
+    data = parse_csv_data('covid19-dashboard/nation_2021-10-28.csv')
     assert len(data) == 639
 
 def test_process_covid_csv_data():
     last7days_cases , current_hospital_cases , total_deaths = \
         process_covid_csv_data ( parse_csv_data (
-            'nation_2021-10-28.csv' ) )
+            'covid19-dashboard/nation_2021-10-28.csv' ) )
     assert last7days_cases == 240_299
     assert current_hospital_cases == 7_019
     assert total_deaths == 141_544
